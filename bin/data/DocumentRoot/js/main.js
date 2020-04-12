74e7e162bf61e6c9de1ce05a9690b42480e4bc02
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-12 13:14:57 -0400

    testing image dl

--- bin/data/DocumentRoot/js/main.js
+++ bin/data/DocumentRoot/js/main.js
@@ -1,15 +1,25 @@
 "use strict";
 
-var liveViewDl;
+var liveView;
 
 function main() {
 
-    liveViewDl = document.getElementById("live_view_dl");
-    liveViewDl.download = "test.jpg";
-
+    liveView = document.getElementById("live_view");
+    liveView.onclick = saveAsPng(liveView);
+    
 }
 
 window.onload = main;
+
+function saveAsPng(img) {
+    var c = document.createElement('canvas');
+    c.width = img.width;
+    c.height = img.height;
+    var ctx = c.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    //window.location = c.toDataURL('image/png');
+    window.open(c.toDataURL('image/png'))
+}
 
 function loadFile(filepath, callback) { 
     // https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript  
